afa2b96d73277937a29e4ba83b16a0dbd0e0b225
jlopez1104 <145376782+jlopez1104@users.noreply.github.com>
2023-10-26 16:45:01 +0000

    done

---
+++ tuple.py
@@ -0,0 +1,26 @@
+#Recall
+
+my_list = ['helloe','hi']
+
+#create tuple
+# use rounded brackets
+
+rndm = ('Ms.Jackson',1958,True)
+# can be indexed
+print(rndm[1])
+#cannot be changed = immutable
+#dictionaries
+#two ways to creat
+#1
+My_dict = dict()
+#2
+my_dict = {}
+
+#dictionary works on a key value pair
+#dictionary of bears
+bear={'grizzly':'brown',
+'black bear':'black',
+'polar':'white',
+'spirit':'white',}
+
+print(bear['polar'])
